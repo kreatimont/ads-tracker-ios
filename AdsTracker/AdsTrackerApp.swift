9fdf467246cfe5c49863ab850bc2aec4117bb6d8
kreatimont <nadtoka.alexandr@gmail.com>
2020-11-06 22:54:27 +0200

    init;

---
+++ AdsTracker/AdsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdsTrackerApp.swift
+//  AdsTracker
+//
+//  Created by Alexandr Nadtoka on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct AdsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
